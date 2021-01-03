ff145de84d041792c6807c5f749bea8f1c80e26b
Zakhary Kaplan <zakharykaplan@gmail.com>
2021-01-03 18:55:00 -0500

    Create src/{demo,timer}.cpp

---
+++ src/demo.cpp
@@ -0,0 +1,48 @@
+//
+//  demo.cpp
+//  Clip demo application.
+//
+//  Created by Zakhary Kaplan on 2020-12-17.
+//  Copyright © 2020 Zakhary Kaplan. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+
+#include "clip/clip.h"
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    // Create parser
+    clip::Parser parser(argc,
+                        argv,
+                        clip::App("demo")
+                            .about("My awesome app. Parsed by clip!")
+                            .author("Zakhary Kaplan <zakharykaplan@gmail.com>")
+                            .version("0.1.0"));
+    // Add parser options
+    parser.add(clip::Opt<double>("double").shortname('d').help("Sample double.").value(0.));
+    parser.add(clip::Opt<int>("int").shortname('i').help("Sample integer.").value(0));
+    parser.add(
+        clip::Opt<string>("string").shortname('s').help("Sample string.").value("Hello, world!"));
+    parser.add(clip::Flag("flag").shortname('f').help("Sample flag."));
+    // Parse args
+    parser.parse();
+
+    // Retrieve args
+    const auto &d = parser.getOpt<double>("double");
+    const auto &i = parser.getOpt<int>("int");
+    const auto &s = parser.getOpt<string>("string");
+    const auto &f = parser.getFlag("flag");
+
+    // Print supplied args
+    if (d.count())
+        cout << d.name << ": .count = " << d.count() << ", .value = " << d.value() << endl;
+    if (i.count())
+        cout << i.name << ": .count = " << i.count() << ", .value = " << i.value() << endl;
+    if (s.count())
+        cout << s.name << ": .count = " << s.count() << ", .value = " << s.value() << endl;
+    if (f.count())
+        cout << f.name << ": .count = " << f.count() << endl;
+}
---
+++ src/timer.cpp
@@ -0,0 +1,69 @@
+//
+//  timer.cpp
+//  Clip demo application.
+//
+//  Created by Zakhary Kaplan on 2021-01-03.
+//  Copyright © 2020 Zakhary Kaplan. All rights reserved.
+//
+
+#include <chrono>
+#include <iostream>
+#include <string>
+#include <thread>
+
+#include "clip/clip.h"
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    // Create parser
+    clip::Parser parser(argc,
+                        argv,
+                        clip::App("timer")
+                            .about("My awesome timer app. Parsed by clip!")
+                            .author("Zakhary Kaplan <zakharykaplan@gmail.com>")
+                            .version("0.1.0"));
+    // Add parser options
+    parser.add(clip::Opt<int>("repeat")
+                   .shortname('n')
+                   .metavar("INT")
+                   .help("Number of times to repeat the delay.")
+                   .value(1));
+    parser.add(clip::Opt<string>("message")
+                   .shortname('m')
+                   .help("Message to print after each delay.")
+                   .metavar("STRING")
+                   .value("Time's up!"));
+    parser.add(clip::Arg<double>("delay").help("Desired delay length (in seconds).").value(0.));
+    parser.add(clip::Flag("verbose").shortname('v').help("Set verbosity level."));
+    // Parse args
+    parser.parse();
+
+    // Retrieve args
+    const auto &delay = parser.getArg<double>("delay");
+    const auto &repeat = parser.getOpt<int>("repeat");
+    const auto &message = parser.getOpt<string>("message");
+    const auto &verbose = parser.getFlag("verbose");
+
+    // Validate args
+    if (delay.value() < 0)
+        clip::Parser::error(1, "delay must be greater than 0");
+    if (repeat.value() < 0)
+        clip::Parser::error(1, "repeat must be greater than 0");
+
+    // Handle verbose output
+    if (verbose.count()) // any verbosity level
+        cout << "Setting " << repeat.value() << " timers for " << delay.value() << " seconds."
+             << endl;
+
+    // Repeat for each timer
+    chrono::duration<double> dur(delay.value());
+    for (int i = 0; i < repeat.value(); i++) {
+        if (verbose.count() >= 2) // verbosity level 2+
+            cout << "Starting timer: " << i << "/" << repeat.value() << "..." << endl;
+
+        this_thread::sleep_for(dur);
+
+        cout << message.value() << endl;
+    }
+}
